e74d40d26725c7b6076239abba21531474b3e9f6
Väisänen, Petri <Petri.Vaisanen@nordicsemi.no>
2021-10-28 10:04:01 -0700

    Pids list added

--- src/system.cpp
+++ src/system.cpp
@@ -20,7 +20,10 @@ using std::vector;
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() {
+  vector<int> pids = LinuxParser::Pids();
+  return processes_;
+}
 
 // Return the system's kernel identifier (string)
 std::string System::Kernel() {
